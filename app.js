af7261e03de1d209cd0b1d096fde18d29751a783
Keith Nocera <knocera@gmail.com>
2019-03-07 13:00:51 -0500

    db and tables done

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const morgan = require('morgan');
 const index = require('./views/index');
 const main = require('./views/main');
 const layout = require('./views/layout');
+const { db } = require('./models');
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
 
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
+
 app.get('/', (req, res, next) => {
-  const page = layout('');
+  const page = layout(' ');
   res.send(page);
 });
 
@@ -22,4 +27,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-
